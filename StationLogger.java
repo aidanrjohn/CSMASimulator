a5dd8ed6ad2b7e167d9b6eb98be534de4853d4e3
Aidan John <ajohn1215@gmail.com>
2018-04-24 13:02:49 -0400

    Add method comments

--- StationLogger.java
+++ StationLogger.java
@@ -4,10 +4,15 @@ public class StationLogger
 	final int DEBUG_LEVEL = 0;
 	final int STANDARD_LEVEL = 1;
 	int currentLogLevel;
+	
+	// Creates a StationLogger object to log station events
 	public StationLogger(int currentLogLevel)
 	{
 		this.currentLogLevel = currentLogLevel;
 	}
+	
+	// Logs a message if the message has the same or higher log level as
+	// the current level
 	public void logMessage(int logLevel, String stationName, String message)
 	{
 		if (logLevel == STANDARD_LEVEL || currentLogLevel == DEBUG_LEVEL)
